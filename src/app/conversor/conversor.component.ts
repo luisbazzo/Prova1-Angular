3b95ee7d0b08695e74fc490e2e90c4f9d3cd46ba
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-04-11 09:04:54 -0300

    Segundo commit

--- src/app/conversor/conversor.component.ts
+++ src/app/conversor/conversor.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./conversor.component.css']
 })
 export class ConversorComponent {
+valorReal : number = 0.20;
+valorDolar : number = 1;
+valorEuro : number = 1.09;
+valorPesoArgentino : number = 0.0047;
 
+valorCambio : number = 0;
+valorConverter : number = 0;
+valorConvertido : number = 0;
 }
